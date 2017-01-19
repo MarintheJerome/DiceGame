7606a5cda41b15002d1dd6eba102d494f8f382a3
MarintheJerome <jerome.marinthe@gmail.com>
2017-01-19 14:02:34 +0100

    lancement vue

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+import view.Vue;
+
+public class Main {
+    public static void main(String[] args){
+        Vue.launch(Vue.class);
+    }
+}
---
+++ src/main/java/view/Vue.java
@@ -0,0 +1,31 @@
+package view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+/**
+ * Created by jerome on 19/01/2017.
+ */
+public class Vue extends Application{
+    public static Scene scene;
+    public static Stage stagePrincipal;
+    public static Pane paneUsed;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader preloader = new FXMLLoader(getClass().getClassLoader().getResource("principale.fxml"));
+        Parent root = preloader.load();
+        scene = new Scene(root,600,400);
+        scene.getStylesheets().clear();
+
+        stage.setTitle("Super jeu de dès");
+        stage.setScene(scene);
+        stagePrincipal = stage;
+        stagePrincipal.setResizable(true);
+        stage.show();
+    }
+}
